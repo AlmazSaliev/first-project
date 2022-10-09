e49b39ac0cca7d2d4ea8ba4605cd8cea3987afde
doolot11 <doolot928@gmail.com>
2022-10-09 20:37:15 +0600

    button done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import './App.css';
+import "./App.css";
+import Button from "./components/UI/Button";
 
 function App() {
   return (
     <div className="App">
+      <h1>hell</h1>
+      <Button />
+      <Button variant='deleted' />
+      <Button variant='contained' />
     </div>
   );
 }
---
+++ src/components/UI/Button.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import ButtonMui from "@mui/material/Button";
+import styled from "@emotion/styled";
+
+function Button({ variant }) {
+  const VARIANTS = {
+    outlined: {
+      border: "1px solid #0b363c",
+      color: "#0b363c",
+      " &:hover": {
+        color: "#fff",
+        backgroundColor: "#0b363c",
+        border: "1px solid #0b363c",
+      },
+    },
+    contained: {
+      border: "1px solid #0b363c",
+      backgroundColor: "#0b363c",
+      color: '#fff',
+      " &:hover": {
+        color: "#fff",
+        backgroundColor: "#0b363c",
+        border: "1px solid #0b363c",
+      },
+    },
+    deleted: {
+        border: "1px solid red",
+        backgroundColor: "#fff",
+        color: 'red',
+        " &:hover": {
+          color: "#fff",
+          backgroundColor: "red",
+          border: "1px solid red",
+        },
+    }
+  };
+
+  return <StyledButton styles={VARIANTS[variant]}>Outlined</StyledButton>;
+}
+Button.defaultProps = {
+  variant: "outlined",
+};
+
+export default Button;
+const StyledButton = styled(ButtonMui)`
+  border-radius: 0;
+  ${({ styles }) => ({ ...styles })}
+`;
