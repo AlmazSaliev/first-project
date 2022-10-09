53982c463308b6ad1af17656514f84d1b89b7db4
doolot11 <doolot928@gmail.com>
2022-10-10 01:04:32 +0600

    registerPage done

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,30 +1,59 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Input from "./UI/Input";
 import Button from "./UI/Button";
 import { styled } from "@mui/material";
+import { useState } from "react";
 
 const Registration = () => {
+  const [gmail, setGmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const storeGmail = "dyikan@gmail.com";
+  const storePass = "123456";
+  const getGmailHandler = (e) => {
+    setGmail(e.target.value);
+  };
+  const getPasswordHandler = (e) => {
+    setPassword(e.target.value);
+  };
+  const goToNextPages = () => {
+    if (gmail === storeGmail && password === storePass) {
+      navigate("/projects");
+    }
+  };
   return (
     <Wrapper>
-      <StyledForm>
+      <StyledForm onSubmit={goToNextPages}>
         <h3>РЕГИСТРАЦИЯ</h3>
-        <Input type="text" placeholder="логин" />
-        <Input type="password" placeholder="пароль" />
-        <Button variant="contained">Регистрация</Button>
+        <Input
+          type="text"
+          value={gmail}
+          onChange={getGmailHandler}
+          placeholder="логин"
+        />
+        <Input
+          type="password"
+          value={password}
+          onChange={getPasswordHandler}
+          placeholder="пароль"
+        />
+        <Button onClick={goToNextPages} variant="contained">
+          Регистрация
+        </Button>
       </StyledForm>
-      <Link to="/projects">Registration</Link>
     </Wrapper>
   );
 };
 export default Registration;
 const Wrapper = styled("div")`
   width: 100%;
-  margin: auto;
+  padding: 15px;
 `;
 const StyledForm = styled("form")`
   text-align: center;
   border-radius: 10px;
-  margin: 50px auto 0 auto;
+  margin: 0 auto;
+  margin-top: 150px;
   padding: 20px;
   border: 1px solid #0b363c;
   max-width: 400px;
@@ -35,8 +64,15 @@ const StyledForm = styled("form")`
   input {
     height: 35px;
     padding: 10px;
+    font-family: "Roboto", sans-serif;
+  }
+  button {
+    &:hover {
+      color: #fff;
+    }
   }
   h3 {
-    color:#0b363c;
+    color: #0b363c;
+    font-family: "Roboto", sans-serif;
   }
 `;
