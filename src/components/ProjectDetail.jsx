96ae50ac55f4b9db0a2a93e84782389bc2151199
kochkanov <makhmud.kochkanov@gmail.com>
2022-10-10 04:06:36 +0600

    done

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -1,15 +1,36 @@
 import styled from '@emotion/styled'
 import Card from './UI/Card'
+import {Floor} from '../constants'
 
 const ProjectDetail = () => {
   return (
-    <Container>
-      <Card />
-    </Container>
+    <WrapperContainer>
+      <Container>
+        {Floor.map((el) => {
+          return (
+            <Card
+              key={el.id}
+              id={el.id}
+              image={el.image}
+              sales={el.sales}
+              booked={el.booked}
+              free={el.free}
+            />
+          )
+        })}
+      </Container>
+    </WrapperContainer>
   )
 }
 export default ProjectDetail
-
+const WrapperContainer = styled('div')`
+  width: 100%;
+`
 const Container = styled('div')`
-  padding: 120px;
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+  margin: 0 auto;
+  margin-top: 100px;
+  gap: 15px;
 `
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,11 +2,13 @@ import styled from '@emotion/styled'
 import React from 'react'
 
 const Card = (props) => {
-  const {image, floor, sales, booked, free} = props
+  const {image, sales, booked, free, id} = props
   return (
     <div>
-      <Total>{floor}-й этаж</Total>
       <CardWrap>
+        <div>
+          <p>{id}-й этаж</p>
+        </div>
         <Section>
           <Img src={image} />
         </Section>
@@ -24,41 +26,47 @@ export default Card
 
 const CardWrap = styled('div')`
   width: 100%;
-  height: 30%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+  height: 80%;
+  position: relative;
+  margin: 0 auto;
+  & > :nth-child(1) {
+    position: absolute;
+    width: 20%;
+    height: 20%;
+    color: darkgreen;
+    top: 10%;
+    right: 23%;
+    & > p {
+      font-size: 2vw;
+    }
+  }
 `
 const Img = styled('img')`
-  width: 70%;
+  width: 100%;
 `
 const Section = styled('div')`
-  margin: 0 0 0 0 auto;
-  width: 50%;
+  margin: 0 auto;
+  width: 100%;
 `
 const Aside = styled('aside')`
+  position: absolute;
+  top: 15%;
+  right: 15%;
   border-radius: 2px solid;
   box-shadow: 0px 5px 10px 2px rgba(34, 60, 80, 0.2);
   width: 30%;
-  height: 30%;
-  padding: 20px;
+  height: 16.5%;
+  padding: 2%;
 `
 const Redp = styled('p')`
-  font-size: 24px;
+  font-size: 2vw;
   color: red;
 `
 const Yellowp = styled('p')`
-  font-size: 24px;
+  font-size: 2vw;
   color: yellow;
 `
 const Bluep = styled('p')`
-  font-size: 24px;
+  font-size: 2vw;
   color: blue;
-`
-
-const Total = styled('p')`
-  font-size: 26px;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  margin-bottom: 20px;
 `
